0510725096b05d31fe428101ff6bbe7b31fa017d
Steve Moyer <smoyer1@selesy.com>
2019-11-28 20:47:36 -0500

    Adds a README
    Resolves #1

---
+++ README.md
@@ -0,0 +1,7 @@
+# uinames-client
+
+A client library to access data provided by the uinames service. (See
+https://uinames.com/#api-panel).  For complete documentation for this Go
+module, see the
+[Go docs](https://godoc.org/github.com/PennState/uinames-client/pkg/uinames)
+or run  ``go doc -all pkg/uinames`` in this repository's top-level folder.
--- pkg/uinames/pkg.go
+++ pkg/uinames/pkg.go
@@ -1,7 +1,8 @@
 /*
 Package uinames provides a client to retrieve fake identities from the
-uinames service (https://uinames.com).  In general, the process involves
-creating a new request with the selected options and then calling the
-Get() function (repeatedly if needed) to retrieve an array of identities.
+uinames service (https://uinames.com/#api-panel).  In general, the process
+involves creating a new request with the selected options and then calling
+the Get() function (repeatedly if needed) to retrieve an array of
+identities.
 */
 package uinames
