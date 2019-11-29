81ac978548feb734ec635416dba04e4787d736bc
Steve Moyer <smoyer1@selesy.com>
2019-11-28 20:37:42 -0500

    Adds package documentation
    Related to #2

---
+++ pkg/uinames/pkg.go
@@ -0,0 +1,7 @@
+/*
+Package uinames provides a client to retrieve fake identities from the
+uinames service (https://uinames.com).  In general, the process involves
+creating a new request with the selected options and then calling the
+Get() function (repeatedly if needed) to retrieve an array of identities.
+*/
+package uinames
